2468ef5e3a4a9880a2708aee697b39e6ebb4e9d1
andyj10224 <ajiang10224@gmail.com>
2020-02-03 16:49:54 -0500

    Added ligand protein files

--- pdb_to_txt_no_comm.py
+++ pdb_to_txt_no_comm.py
@@ -8,7 +8,7 @@ Created on Sat Feb  1 20:06:10 2020
 
 pdbFile = open("6qlt.pdb", "r")
 
-txtFile = open("6qlt_coord_protein.xyz", "w")
+txtFile = open("6qlt_coord_protein_no_comm.xyz", "w")
 
 pdb_lines = pdbFile.readlines()
 
@@ -24,13 +24,13 @@ for i in range(len(pdb_lines)):
 
 for j in range(len(atom_lines)):
     l = atom_lines[j]
-    txtFile.write(l[76:78] + " " + l[30:38] + " " + l[38:46] + " " + l[46:54] + "  #" + "AA: " + l[17:20] + " (" + l[22:26] + " )" +  "\n")
+    txtFile.write(l[76:78] + " " + l[30:38] + " " + l[38:46] + " " + l[46:54] + "\n")
 
 ligand_files = []
 
 count = 0
 
-temp = open("ligand0.xyz", "w")
+temp = open("ligand0_no_comm.xyz", "w")
 
 ligand_files.append(temp)
 
@@ -38,10 +38,10 @@ for k in range(len(hetatm_lines)):
     het = hetatm_lines[k]
     if k >= 1 and het[22:26] != hetatm_lines[k-1][22:26]:
         count = count + 1
-        temp = open("ligand" + str(count) + ".xyz", "w")
+        temp = open("ligand" + str(count) + "_no_comm.xyz", "w")
         ligand_files.append(temp)
 
-    ligand_files[count].write(het[76:78] + " " + het[30:38] + " " + het[38:46] + " " + het[46:54] + "#(" + het[22:26] + " )" +  "\n")
+    ligand_files[count].write(het[76:78] + " " + het[30:38] + " " + het[38:46] + " " + het[46:54] + "\n")
  
 
 pdbFile.close()
